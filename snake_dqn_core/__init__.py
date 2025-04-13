91914ab16b98ae5cd1ed38c73cb1f2b94d2e2100
LacyCat <166228777+LacyCat@users.noreply.github.com>
2025-04-13 21:06:20 +0900

    .

--- snake_dqn_core/__init__.py
+++ snake_dqn_core/__init__.py
@@ -3,4 +3,5 @@ from .play_model import play_dqn_model, play_doubling_dqn_model, play_dueling_dq
 from .train_dqn import train_dqn,DQN, ReplayBuffer
 from .train_doubling_dqn import train_doubling_dqn, Doubling_DQN, ReplayBuffer
 from .train_dueling_dqn import train_dueling_dqn, Dueling_DQN, ReplayBuffer
-from .train_hybrid_dqn import train_hybrid_dqn, Hybrid_DQN, ReplayBuffer
+from .train_hybrid_dqn import train_hybrid_dqn, Hybrid_DQN, PrioritizedReplayBuffer
+from .train_rainbow_dqn import train_rainbow_dqn, Rainbow_DQN, PrioritizedReplayBuffer
